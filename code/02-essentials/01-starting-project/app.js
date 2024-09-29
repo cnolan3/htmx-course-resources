9091e0c1359b0caae760b4381ca99552e203e52e
Connor Nolan <connor@cnolandev.com>
2024-09-29 12:29:36 -0700

    (lecture 2-17) essentials starting project: use 'hx-select' with redirect to replace the list

--- code/02-essentials/01-starting-project/app.js
+++ code/02-essentials/01-starting-project/app.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 
         <main>
           <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
-          <form hx-post="/note" hx-target="ul" hx-swap="outerHTML">
+          <form hx-post="/note" hx-target="ul" hx-swap="outerHTML" hx-select="ul">
             <p>
               <label for="note">Your note</label>
               <input type="text" id="note" name="note">
@@ -53,12 +53,12 @@ app.post("/note", (req, res) => {
 
   HTMX_KNOWLEDGE.unshift(enteredNote);
 
-  // res.redirect("/");
-  res.send(`
-    <ul>
-      ${HTMX_KNOWLEDGE.map((info) => `<li>${info}</li>`).join("")}
-    </ul>
-`);
+  res.redirect("/");
+  //   res.send(`
+  //     <ul>
+  //       ${HTMX_KNOWLEDGE.map((info) => `<li>${info}</li>`).join("")}
+  //     </ul>
+  // `);
 });
 
 app.listen(3000);
